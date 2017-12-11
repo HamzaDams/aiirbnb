ddb82cb6e5a89f9d1161b594ab23e41bb5c30a35
Hamza Damene <hddesignx@gmail.com>
2017-12-11 15:48:49 +0000

    Ajax datepickers

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,21 +1,33 @@
 class ReservationsController < ApplicationController
     
     before_action :authenticate_user!
-    def preload
-        room = Room.find(params[:room_id])
-        today = Date.today
-        reservations = room.reservations.where("str_date >= ? OR end_date >= ?, today, today")
+      def preload
+           room = Room.find(params[:room_id])
+           today = Date.today
+           reservations = room.reservations.where("str_date >= ? OR end_date >= ?", today, today)
+           
+           render json: reservations
+        end
         
-        render json: reservations
-    end
-    def create
-        @reservation = current_user.reservations.create(reservation_params)
-        redirect_to @reservation.room, notice: "Votre réservation a été acceptée"
-    end
-    
-    
-   private 
-   def reservation_params
-       params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
-    end
+        def preview
+            str_date = Date.parse(params[:str_date])
+            end_date = Date.parse(params[:end_date])
+            
+            output = {
+                conflict: is_conflict(str_date, end_date)
+            }
+            
+            render json: output
+        end
+     
+    private
+        def is_conflict(str_date, end_date)
+            room = Room.find(params[:room_id])
+     
+            check = room.reservations.where("? < str_date AND end_date < ?", str_date, end_date)
+            check.size > 0? true : false
+        end
+       def reservation_params
+           params.require(:reservation).permit(:str_date, :end_date, :price, :total, :room_id)
+        end
 end
